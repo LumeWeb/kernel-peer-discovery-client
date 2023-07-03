fa5ec4448910fd96ff0efa40281859d830205c79
Derrick Hammer <derrick@derrickhammer.com>
2023-07-03 05:24:41 -0400

    dep: update module hash

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Client, factory } from "@lumeweb/libkernel/module";
 import type { Peer } from "@lumeweb/libpeerdiscovery";
 import { hexToBuf } from "@lumeweb/libweb";
 
-const MODULE = "zduHR9bx43UsjTQomS9K2AzvjRVuh3YjqaMvS32FUKVYx3sCoBsibakc8w";
+const MODULE = "zduQZvN3rpSKUBDfhQLjTRP22dfAL6YXYxrPNLhw1jCDEMGnxDWtzocLvT";
 
 export class PeerDiscoveryClient extends Client {
   public async register(source: string): Promise<void> {
